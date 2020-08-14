e58aa214273032361c51e0682db5e24d92e771bf
avaneeshtripathi <avaneeshtripathi93@gmail.com>
2020-08-14 20:20:02 +0530

    fix: rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,28 @@
-import sass from 'rollup-plugin-sass';
-import typescript from 'rollup-plugin-typescript2';
-import bundleSize from 'rollup-plugin-bundle-size';
 import { terser } from "rollup-plugin-terser";
+import sass from 'rollup-plugin-sass';
+import bundleSize from 'rollup-plugin-bundle-size';
+import cleanup from 'rollup-plugin-cleanup';
+import typescript from 'rollup-plugin-typescript2';
+
+import pkg from './package.json';
 
 export default {
-    input: 'src/index.tsx',
-    output: [
-      {
-        file: 'dist/index.js',
-        format: 'cjs',
-        exports: 'named',
-        sourcemap: 'inline',
-        strict: false,
-        compact: true
-      }
-    ],
-    plugins: [
-      sass({ insert: true }),
-      typescript({ objectHashIgnoreUnknownHack: false }),
-      terser(),
-      bundleSize(),
-    ],
-    external: ['react', 'react-dom']
+  input: 'src/index.tsx',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+      strict: false
+    }
+  ],
+  plugins: [
+    sass({ insert: true }),
+    typescript({ objectHashIgnoreUnknownHack: false }),
+    cleanup(),
+    terser(),
+    bundleSize(),
+  ],
+  external: ['react', 'react-dom']
 }
