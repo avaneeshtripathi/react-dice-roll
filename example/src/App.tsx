4eccb3c2c7abba6b7cd9bb85da3c086c04d1056b
glinda93 <72871001+glinda93@users.noreply.github.com>
2021-12-25 00:41:54 +0900

    add example

---
+++ example/src/App.tsx
@@ -0,0 +1,13 @@
+import Dice from 'react-dice-roll';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Dice Roll</h1>
+      <Dice cheatValue={2} />
+    </div>
+  );
+}
+
+export default App;
